f9410ed7bc66b6a09eb9b26c77e4da517a5ffb9e
sandrofunk <sandro.funk@bbchain.com.br>
2022-02-08 20:51:35 -0300

    Create UserServiceImpl

---
+++ src/main/java/com/api/user/my/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.api.user.my.api.services.impl;
+
+import com.api.user.my.api.domain.User;
+import com.api.user.my.api.repositories.UserRepository;
+import com.api.user.my.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+
+
+}
