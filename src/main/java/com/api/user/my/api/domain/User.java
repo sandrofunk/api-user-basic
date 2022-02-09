2d3feead9fbc11b22633b626110c139180bc624b
sandrofunk <sandro.funk@bbchain.com.br>
2022-02-09 08:23:16 -0300

    files

---
+++ src/main/java/com/api/user/my/api/domain/User.java
@@ -0,0 +1,26 @@
+package com.api.user.my.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String user;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
