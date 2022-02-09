f25633a3103dc480a80af7075065f4ff97af35b6
sandrofunk <sandro.funk@bbchain.com.br>
2022-02-08 21:26:06 -0300

    file update

--- src/main/java/com/api/user/my/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/api/user/my/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.api.user.my.api.services.impl;
 import com.api.user.my.api.domain.User;
 import com.api.user.my.api.repositories.UserRepository;
 import com.api.user.my.api.services.UserService;
+import com.api.user.my.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado"));
     }
-
-
 }
