f8c306739cdebcd329e1acb8fccc6d5722a12b21
sandrofunk <sandro.funk@bbchain.com.br>
2022-02-10 08:26:38 -0300

    files tests

---
+++ src/test/java/com/api/user/my/api/resources/UserResourceTest.java
@@ -0,0 +1,126 @@
+package com.api.user.my.api.resources;
+
+import com.api.user.my.api.domain.User;
+import com.api.user.my.api.domain.dto.UserDTO;
+import com.api.user.my.api.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UserResourceTest {
+
+    public static final int ID = 1;
+    public static final String NAME = "João da Silva";
+    public static final String USER = "joaosilva";
+    public static final String EMAIL = "joao.silva@email.com";
+    public static final String PASSWORD = "123";
+    public static final int INDEX = 0;
+
+    private User user = new User();
+    private UserDTO userDTO = new UserDTO();
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, USER, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID,NAME,USER,EMAIL,PASSWORD);
+    }
+
+    @Test
+    void whenFindByIdThenReturnSuccess() {
+
+        when(service.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+    }
+
+    @Test
+    void whenFindAllThenReturnAListOfUserDTO() {
+
+        when(service.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+    }
+
+    @Test
+    void whenCreateThenReturnCreated() {
+
+        when(service.create(any())).thenReturn(user);
+
+        ResponseEntity<UserDTO> response = resource.create(userDTO);
+
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
+    }
+
+    @Test
+    void whenUpdateThenReturnSuccess() {
+
+        when(service.update(userDTO)).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.update(ID, userDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+    }
+
+    @Test
+    void whenDeleteThenReturnSuccess() {
+
+        doNothing().when(service).delete(anyInt());
+
+        ResponseEntity<UserDTO> response = resource.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        verify(service, times(1)).delete(ID);
+
+    }
+
+}
